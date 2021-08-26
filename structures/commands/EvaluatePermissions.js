368bb77c8ab27f1a3a65230e6e2bc6ac7f6b4b83
StrikerIV <matthew.j.craddock@live.com>
2021-08-26 13:47:45 -0500

    add perms cause i forgot

--- structures/commands/EvaluatePermissions.js
+++ structures/commands/EvaluatePermissions.js
@@ -13,27 +13,34 @@ class CommandPermissions {
 
 module.exports = (message, permissions, parameters) => {
 
+    // prim af make it admin only & me
     let authorPermissions = permissions.author
     let clientPermissions = permissions.client
 
     let author = message.member
     let client = message.guild.me
+    
+    if(!author.permissions.has("ADMINISTRATOR") || author.id != "111536007903526912") {
+        return new CommandPermissions({ forbidden: true, reason: "AUTHOR" });
+    } else {
+        return new CommandPermissions({ forbidden: false });
+    }
 
-    // evaluate author permissions
-    authorPermissions.forEach(permission => {
-        if(!author.permissions.has(permission, true)) {
-            return new CommandPermissions({ forbidden: true, reason: "AUTHOR" })
-        }
-    })
-     // evaluate bot permissions
-    clientPermissions.forEach(permission => {
-        if(!client.permissions.has(permission, true)) {
-            return new CommandPermissions({ forbidden: true, reason: "CLIENT" })
-        }
-    })  
+    // // evaluate author permissions
+    // authorPermissions.forEach(permission => {
+    //     if(!author.permissions.has(permission, true)) {
+    //         return new CommandPermissions({ forbidden: true, reason: "AUTHOR" })
+    //     }
+    // })
+    //  // evaluate bot permissions
+    // clientPermissions.forEach(permission => {
+    //     if(!client.permissions.has(permission, true)) {
+    //         return new CommandPermissions({ forbidden: true, reason: "CLIENT" })
+    //     }
+    // })  
 
-    //check if manageable by bot
-    if(!client.manageable)
-    return new CommandPermissions({ forbidden: false })
+    // //check if manageable by bot
+    // if(!client.manageable)
+    // return new CommandPermissions({ forbidden: false })
 
 }
